9972164b278e8e81bca06f24b370ba6e27b31f95
Francisco Costa <francisco@fcosta.pt>
2019-03-19 12:10:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,12 +9,13 @@ def countdown (seconds)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num)
-  while number >0
-  puts "#{number} SECOND(S)!"
-  number -=1
-  sleep 1
-end
- 'HAPPY NEW YEAR!'
+def countdown_with_sleep(seconds)
+
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 
 end
